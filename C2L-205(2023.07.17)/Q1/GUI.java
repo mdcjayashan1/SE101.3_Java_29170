1d601c69f13070b4633d16b62461cd4871dcc9d4
mdcjayashan1 <137048029+mdcjayashan1@users.noreply.github.com>
2023-07-17 12:57:10 +0530

    Add files via upload

---
+++ C2L-205(2023.07.17)/Q1/GUI.java
@@ -0,0 +1,15 @@
+package com.mycompany.gui;
+
+/**
+ *
+ * @author M D C Jayashan
+ */
+public class GUI 
+{
+
+    public static void main(String[] args) 
+    {
+        color n=new color();
+        n.show();
+    }
+}
---
+++ C2L-205(2023.07.17)/Q2/GUI1.java
@@ -0,0 +1,15 @@
+package com.mycompany.gui1;
+
+/**
+ *
+ * @author M D C Jayashan
+ */
+public class GUI1 
+{
+
+    public static void main(String[] args) 
+    {
+        Counter c=new Counter();
+        c.show();
+    }
+}
